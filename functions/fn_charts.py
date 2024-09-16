b788f292986b04fb0a8f80664323d8f8913a2f79
Matheus Resende <75645023+matheusresende7@users.noreply.github.com>
2024-09-16 16:12:00 -0300

    v0.12

---
+++ functions/fn_charts.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+
+def boxplots( # Função para criar gráficos boxplot
+    df, # Passando o df como parâmetro da função
+    columns, # Passando as colunas como parâmetro da função
+): 
+    columns = columns # Passando as colunas que vão ser usadas para criar os boxplots
+
+    fig, axs = plt.subplots( # Criando a figura para colocar os gráficos de boxplots
+        nrows=1, # Definindo o número de linhas
+        ncols=3, # Definindo o número de colunas
+        figsize=(12,4), # Definindo o tamanho da figura
+        tight_layout=True # Definindo o layout dos gráficos
+    )
+
+    for i, column in enumerate(columns): # Criando uma estrutura de repetição para criar cada um dos boxplots
+        sns.boxplot( # Criando o boxplot
+            y=column, # Passando a coluna de referência para criar o boxplot
+            data=df, # Passando o dataframe para criar o boxplot
+            ax=axs[i] # Passando a posição do gráfico dentro da plotagem do matplotlib
+        )
+
+    plt.show() # Exibindo os boxplots
+
+
+
+    
