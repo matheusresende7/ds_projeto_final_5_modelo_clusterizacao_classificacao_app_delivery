1cdc2b82de5d9615361fcb8cec265f1f7ad11340
Matheus Resende <75645023+matheusresende7@users.noreply.github.com>
2024-09-17 16:31:03 -0300

    v0.14

--- functions/fn_charts.py
+++ functions/fn_charts.py
@@ -6,7 +6,8 @@ import seaborn as sns
 
 def boxplots( # Função para criar gráficos boxplot
     df, # Passando o df como parâmetro da função
-    columns, # Passando as colunas como parâmetro da função
+    columns, # Passando as colunas Y como parâmetro da função
+    x = None # Passando as colunas X como parâmetro da função
 ): 
     columns = columns # Passando as colunas que vão ser usadas para criar os boxplots
 
@@ -19,9 +20,11 @@ def boxplots( # Função para criar gráficos boxplot
 
     for i, column in enumerate(columns): # Criando uma estrutura de repetição para criar cada um dos boxplots
         sns.boxplot( # Criando o boxplot
+            x=x, # Passando a coluna x que permite criar comparações em relação a alguma variável (geralmente categórica)
             y=column, # Passando a coluna de referência para criar o boxplot
             data=df, # Passando o dataframe para criar o boxplot
             ax=axs[i], # Passando a posição do gráfico dentro da plotagem do matplotlib
+            showmeans=True, # Exibindo a média no boxplot, através de um triângulo verde
         )
         axs[i].set_title(f'Boxplot - {column}', fontsize=12)  # Adiciona um título para cada boxplot
         axs[i].set_ylabel('') # Removendo o título do eixo Y
@@ -30,4 +33,20 @@ def boxplots( # Função para criar gráficos boxplot
 
 
 
+def pairplots( # Função para criar gráficos pairplots
+    df, # Passando o df como parâmetro da função
+    columns, # Passando as colunas como parâmetro da função
+    hue = None, # Passando o hue como parâmetro da função
+    alpha = 0.5, # Passando o alpha como parâmetro da função
+    corner = True, # Passando o corner como parâmetro da função
+):
     
+    analysis_columns = columns.copy() + [hue] # Adicionando a coluna hue as colunas que vão ser analisadas
+    
+    sns.pairplot( # Criando o gráfico pairplots
+        df[analysis_columns], # Passando as colunas que serão usadas nos pairplots
+        diag_kind="kde", # Definindo o tipo curva
+        hue=hue, # Definindo o parâmetro hue (legenda)
+        plot_kws=dict(alpha=alpha), # Definindo a transparência da plotagem dos gráficos
+        corner=corner, # Definindo o pairplots apenas na parte espelhada inferior
+    )
--- params/consts.py
+++ params/consts.py
@@ -1,10 +1,14 @@
 # Data
-DATABASE = '../data/raw/database.csv'
+RAW_DATABASE = '../data/raw/raw_database.csv'
 PROCESSED_DATABASE = '../data/processed/processed_database.csv'
 
 # Images
 WALLPAPER = '../images/illustrations/wallpaper.png'
 SCHEDULE = '../images/outputs/schedule.png'
+
+# Reports
+EDA_0 = '../reports/eda_0.html'
+EDA_1 = '../reports/eda_1.html'
 
 # Values
 RANDOM_STATE = 42
