18309924e31f05215d3da0109613ed3a6edc8e2d
Matheus Resende <75645023+matheusresende7@users.noreply.github.com>
2026-06-06 20:47:03 -0300

    v1.11

--- params/consts.py
+++ params/consts.py
@@ -14,6 +14,15 @@ DATASET_CLEAN_PYSPARK = '../data/03_silver/dataset_clean_pyspark'
 DATASET_CLUSTERED_PANDAS = '../data/03_silver/dataset_clustered_pandas.parquet'
 DATASET_CLUSTERED_PYSPARK = '../data/03_silver/dataset_clustered_pyspark'
 
+DATASET_MODELS_METRICS_PANDAS = '../data/04_gold/dataset_models_metrics_pandas.csv'
+DATASET_MODELS_METRICS_PYSPARK = '../data/04_gold/dataset_models_metrics_pyspark'
+
+DATASET_MODEL_GROWTH_PANDAS = '../data/04_gold/dataset_model_growth_pandas.csv'
+DATASET_MODEL_GROWTH_PYSPARK = '../data/04_gold/dataset_model_growth_pyspark'
+
+DATASET_COMPARING_METRICS_PANDAS = '../data/04_gold/dataset_comparing_metrics_pandas.csv'
+DATASET_COMPARING_METRICS_PYSPARK = '../data/04_gold/dataset_comparing_metrics_pyspark'
+
 # Models
 GRID_SEARCH_CLASSIFICATION_PANDAS_JOBLIB = '../models/grid_search_classification_pandas.joblib'
 GRID_SEARCH_CLASSIFICATION_PYSPARK_JOBLIB = '../models/grid_search_classification_pyspark.joblib'
